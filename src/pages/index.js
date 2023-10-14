726316268cfa2d699b89bc62efc734072a91b261
patdel0 <zefiru@protonmail.com>
2023-10-14 23:19:24 +0100

    Add solution to problem

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,47 @@
+import { useEffect, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+
 const Home = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/users");
+      const data = await res.json();
+      const formattedUsers = data.data.users.map((user) => ({
+        id: user.id,
+        name: user.attributes.name,
+        age: user.attributes.age,
+        email: user.attributes.contact.email,
+        role: user.attributes.role,
+        score: user.attributes.score,
+      }));
+      setUsers(formattedUsers);
+    };
+
+    fetchData();
+  }, []);
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    { field: "name", headerName: "Name", width: 150 },
+    { field: "age", headerName: "Age", width: 110 },
+    {
+      field: "email",
+      headerName: "Email",
+      width: 200,
+      renderCell: (params) => (
+        <a href={`mailto:${params.value}`}>{params.value}</a>
+      ),
+    },
+    { field: "role", headerName: "Role", width: 110 },
+    { field: "score", headerName: "Score", width: 110 },
+  ];
   return (
-    <h1>Hello World!</h1>
+    <div style={{ height: 400, width: "100%" }}>
+      <h1>User Data</h1>
+      <DataGrid rows={users} columns={columns} pageSize={5} />
+    </div>
   );
 };
 
